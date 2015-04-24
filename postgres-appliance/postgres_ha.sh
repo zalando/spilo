42e1d9e678581d0ce726920900b6464ebfd3621f
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2015-04-24 16:28:31 +0200

    remove inline patch for the governor and switched the source to the zalando fork.

--- postgres-appliance/postgres_ha.sh
+++ postgres-appliance/postgres_ha.sh
@@ -2,27 +2,6 @@
 
 PATH=$PATH:/usr/lib/postgresql/${PGVERSION}/bin
 
-
-function patch_governor
-{
-#!/bin/bash
-cat << EOF |patch governor/helpers/postgresql.py
-diff --git a/helpers/postgresql.py b/helpers/postgresql.py
-index b6c52c1..4d3bfb1 100644
---- a/helpers/postgresql.py
-+++ b/helpers/postgresql.py
-@@ -146,6 +146,8 @@ class Postgresql:
-         f = open("%s/pg_hba.conf" % self.data_dir, "a")
-         f.write("host replication %(username)s %(network)s md5" %
-                 {"username": self.replication["username"], "network": self.replication["network"]})
-+        # allow TCP connections from the host's own address
-+        f.write("\nhost postgres postgres %(network)s/32 trust\n" % {"network": self.host})
-         f.close()
- 
-     def write_recovery_conf(self, leader_hash):
-EOF
-}
-
 function write_postgres_yaml
 {
   local_address=$(cat /etc/hosts |grep ${HOSTNAME}|cut -f1)
@@ -52,9 +31,7 @@ __EOF__
 }
 
 # get governor code
-git clone https://github.com/compose/governor.git
-
-patch_governor
+git clone https://github.com/zalando/governor.git
 
 write_postgres_yaml
 
