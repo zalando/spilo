adad58f7281c8f0a5a16ac0fc3597273703be769
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2015-05-13 15:00:11 +0200

    Support WAL shipping through WAL-e, remove the obsolete ETCD_ADDRESS parameter in favor of discovery URL.

--- postgres-appliance/postgres_ha.sh
+++ postgres-appliance/postgres_ha.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PATH=$PATH:/usr/lib/postgresql/${PGVERSION}/bin
+WALE_ENV_DIR=/home/postgres/etc/wal-e.d/env
 
 function write_postgres_yaml
 {
@@ -28,13 +29,21 @@ postgresql:
   parameters:
     archive_mode: "on"
     wal_level: hot_standby
-    archive_command: /bin/true
+    archive_command: "envdir ${WALE_ENV_DIR} wal-e --aws-instance-profile wal-push \"%p\" -p 1"
     max_wal_senders: 5
     wal_keep_segments: 8
     archive_timeout: 1800s
     max_replication_slots: 5
     hot_standby: "on"
+  recovery_conf:
+    restore_command: "envdir ${WALE_ENV_DIR} wal-e --aws-instance-profile wal-fetch \"%f\" \"%p\" -p 1"
 __EOF__
+}
+
+function write_archive_command_environment
+{
+  mkdir -p ${WALE_ENV_DIR}
+  echo "s3://${WAL_S3_BUCKET}/spilo/${SCOPE}/wal/" > ${WALE_ENV_DIR}/WALE_S3_PREFIX
 }
 
 # get governor code
@@ -42,17 +51,13 @@ git clone https://github.com/zalando/governor.git
 
 write_postgres_yaml
 
+write_archive_command_environment
+
 # start etcd proxy
 # for the -proxy on TDB the url of the etcd cluster
 [ "$DEBUG" -eq 1 ] && exec /bin/bash
 
-if [[ -n $ETCD_ADDRESS ]]
-then
-  # address is still useful for local debugging
-  etcd -name "proxy-$SCOPE" -proxy on -bind-addr 127.0.0.1:8080 --data-dir=etcd -initial-cluster $ETCD_ADDRESS &
-else
-  etcd -name "proxy-$SCOPE" -proxy on -bind-addr 127.0.0.1:8080 --data-dir=etcd -discovery-srv $ETCD_DISCOVERY_URL &
-fi
+etcd -name "proxy-$SCOPE" -proxy on -bind-addr 127.0.0.1:8080 --data-dir=etcd -discovery-srv $ETCD_DISCOVERY_URL &
 
 exec governor/governor.py "/home/postgres/postgres.yml"
 
