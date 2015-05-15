691b84a01ff837a7168ab9fe53b35c6295dc3f85
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2015-05-15 16:24:47 +0200

    Add a bash background loop to periodically check and create the backups. Still missing the part to run it at the specified time.

--- postgres-appliance/postgres_ha.sh
+++ postgres-appliance/postgres_ha.sh
@@ -3,6 +3,8 @@
 PATH=$PATH:/usr/lib/postgresql/${PGVERSION}/bin
 WALE_ENV_DIR=/home/postgres/etc/wal-e.d/env
 
+BACKUP_INTERVAL=300
+
 function write_postgres_yaml
 {
   aws_private_ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
@@ -57,7 +59,39 @@ write_archive_command_environment
 # for the -proxy on TDB the url of the etcd cluster
 [ "$DEBUG" -eq 1 ] && exec /bin/bash
 
-etcd -name "proxy-$SCOPE" -proxy on -bind-addr 127.0.0.1:8080 --data-dir=etcd -discovery-srv $ETCD_DISCOVERY_URL &
+# resurrect etcd if it's gone
+(
+  while true
+  do
+    etcd -name "proxy-$SCOPE" -proxy on -bind-addr 127.0.0.1:8080 --data-dir=etcd -discovery-srv $ETCD_DISCOVERY_URL
+  done
+) &
+
+# run wal-e s3 backup periodically
+# XXX: for debugging purposes, it's running every 5 minutes
+(
+  RETRY=1
+  while true
+  do
+    sleep 5
+
+    CURRENT_TS=$(date +%s)
+    pg_isready >/dev/null 2>&2 || continue
+    IN_RECOVERY=$(psql -tqAc "select pg_is_in_recovery()")
+
+    [[ $IN_RECOVERY != "f" ]] && echo "still in recovery" && continue
+    # get the time since the last backup
+    LAST_BACKUP_TS=$(envdir ${WALE_ENV_DIR} wal-e --aws-instance-profile  backup-list LATEST 2>/dev/null|tail -n1|awk '{print $2}'|xargs date +%s --date)
+    ELAPSED_TIME=$((CURRENT_TS-LAST_BACKUP_TS))
+    ([[ $RETRY != 0 ]] || [[ $ELAPSED_TIME -lt $BACKUP_INTERVAL ]]) && continue
+    # leave only 2 base backups before creating a new one
+    envdir ${WALE_ENV_DIR} wal-e --aws-instance-profile delete --confirm retain 2
+    # push a new base backup
+    echo "producing a new backup at $(date)"
+    envdir ${WALE_ENV_DIR} wal-e --aws-instance-profile backup-push ${PGDATA}
+    RETRY=$?
+  done
+) &
 
 exec governor/governor.py "/home/postgres/postgres.yml"
 
