6fd7b6775e7d129b2041b64dcab3e60f685e6b4d
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2015-08-25 10:49:07 +0200

    Make tests PEP3-compatible as well.

--- spilo_cmd/tests/test_cli.py
+++ spilo_cmd/tests/test_cli.py
@@ -6,8 +6,10 @@ from spilo.spilo import cli, process_options, print_spilos, tunnel, get_tunnel
 
 Spilo = collections.namedtuple('Spilo', 'stack_name, version, dns, elb, instances, vpc_id, stack')
 
+
 def test_cli():
     cli
+
 
 def test_tunnel():
     arguments = []
@@ -23,22 +25,25 @@ def test_tunnel():
     result = runner.invoke(tunnel, options)
     assert result.exit_code == -1
     assert 't3st' in str(result.exception)
-    
+
     arguments = ['abc']
     result = runner.invoke(tunnel, arguments)
     assert result.exit_code != 0
 
+
 def test_list():
     pass
 
+
 def test_option_processing():
     process_options(opts=None)
-    process_options(opts={'loglevel':'DEBUG', 'cluster':'feike', 'odd_config_file':'~/.config/piu/piu.yaml'})
+    process_options(opts={'loglevel': 'DEBUG', 'cluster': 'feike', 'odd_config_file': '~/.config/piu/piu.yaml'})
+
 
 def test_print_spilos():
     spilos = list()
     print_spilos(spilos)
-    
+
     spilos.append(Spilo(None, None, None, None, None, None, None))
     print_spilos(spilos)
 
