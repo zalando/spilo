8e255233bb0afee027f04a53a1fc7a5a9ad6ba04
Joar Wandborg <joar@wandborg.se>
2017-04-28 10:31:20 +0200

    Can't mix manual and automatic field numbering

--- postgres-appliance/configure_spilo.py
+++ postgres-appliance/configure_spilo.py
@@ -484,7 +484,7 @@ def main():
     user_config = yaml.load(os.environ.get('SPILO_CONFIGURATION', os.environ.get('PATRONI_CONFIGURATION', ''))) or {}
     if not isinstance(user_config, dict):
         config_var_name = 'SPILO_CONFIGURATION' if 'SPILO_CONFIGURATION' in os.environ else 'PATRONI_CONFIGURATION'
-        raise ValueError('{0} should contain a dict, yet it is a {}'.format(config_var_name, type(user_config)))
+        raise ValueError('{0} should contain a dict, yet it is a {1}'.format(config_var_name, type(user_config)))
 
     config = deep_update(user_config, config)
 
