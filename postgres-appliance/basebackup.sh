c312d2319a73014ec8c89c241b0bb8a0c0190792
Oleksii Kliukin <porkbird@icloud.com>
2017-05-17 14:07:20 +0200

    Wait at least 1s for pg_receivexlog to exit.
    Also avoid removing files recursively. Code review.

--- postgres-appliance/basebackup.sh
+++ postgres-appliance/basebackup.sh
@@ -37,7 +37,7 @@ function receivexlog() {
         kill -0 $receivexlog_pid && sleep 1 || exit
     done
 
-    kill $receivexlog_pid && rm -fr ${XLOG_FAST}/*
+    kill $receivexlog_pid && sleep 1 && rm -f ${XLOG_FAST}/*
 }
 
 ATTEMPT=0
