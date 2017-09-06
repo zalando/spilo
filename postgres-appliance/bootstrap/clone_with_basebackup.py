46863ffb77c55927ff2902ce6b6bd70e1d8767e7
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2017-09-06 18:14:16 +0200

    Slight wording change.

--- postgres-appliance/bootstrap/clone_with_basebackup.py
+++ postgres-appliance/bootstrap/clone_with_basebackup.py
@@ -99,7 +99,7 @@ def run_basebackup(options):
     pg_basebackup = os.path.join(options.bindir, 'pg_basebackup')
     try:
         connstr, env = prepare_connection(options)
-        logger.info("cloning cluster {0} using \"{1}\"".format(options.name, connstr))
+        logger.info("cloning cluster {0} from \"{1}\"".format(options.name, connstr))
         ret = subprocess.call([pg_basebackup, '--pgdata={0}'.format(options.datadir), '-X', 'stream', '--dbname={0}'.format(connstr), '-w'], env=env)
         if ret != 0:
             raise Exception("pg_basebackup exited with code={0}".format(ret))
