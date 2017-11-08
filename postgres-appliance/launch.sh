2cfa0303f0e9719b1c61ca79af0dbe49d2b2179c
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2017-11-08 16:49:19 +0100

    Don't install cron and supervisord in demospilo
    And start patroni with pid=1 instead of supervisord

--- postgres-appliance/launch.sh
+++ postgres-appliance/launch.sh
@@ -1,23 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
 cd /
 tar -xpJf a.tar.xz
 rm a.tar.xz
 
-pgrep supervisord > /dev/null
-if [ $? -ne 1 ]; then echo "ERROR: Supervisord is already running"; exit 1; fi
+if [ "$DEMO" != "true" ]; then
+    pgrep supervisord > /dev/null && echo "ERROR: Supervisord is already running" && exit 1
+fi
 
-mkdir -p "$PGLOG" && chown -R postgres:postgres "$PGROOT"
+mkdir -p "$PGLOG"
 
 ## Ensure all logfiles exist, most appliances will have
 ## a foreign data wrapper pointing to these files
-for i in $(seq 0 7); do touch "${PGLOG}/postgresql-$i.csv"; done
-chown -R postgres:postgres "$PGLOG"
+for i in $(seq 0 7); do
+    if [ ! -f "${PGLOG}/postgresql-$i.csv" ]; then
+        touch "${PGLOG}/postgresql-$i.csv"
+    fi
+done
+chown -R postgres:postgres "$PGROOT"
 
-if python3 /configure_spilo.py all; then
+if [ $DEMO = "true" ]; then
+    python3 /configure_spilo.py patroni patronictl certificate pam-oauth2
     (
-        su postgres -c "/patroni_wait.sh -t 3600 -- /postgres_backup.sh $WALE_ENV_DIR $PGDATA"
+        su postgres -c 'env -i PGAPPNAME="pgq ticker" /patroni_wait.sh --role master -- /usr/local/bin/pgqd /home/postgres/pgq_ticker.ini'
     ) &
+    exec su postgres -c 'exec patroni /home/postgres/postgres.yml'
+else
+    if python3 /configure_spilo.py all; then
+        (
+            su postgres -c "/patroni_wait.sh -t 3600 -- /postgres_backup.sh $WALE_ENV_DIR $PGDATA"
+        ) &
+    fi
+    exec supervisord --configuration=/etc/supervisor/supervisord.conf --nodaemon
 fi
-
-exec supervisord --configuration=/etc/supervisor/supervisord.conf --nodaemon
