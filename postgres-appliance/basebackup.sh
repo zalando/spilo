5c651e9f64973f6ebdaa32f69ca60868bf754f5d
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2017-11-17 09:41:28 +0100

    Make basebackup.sh postgres-10 compatible

--- postgres-appliance/basebackup.sh
+++ postgres-appliance/basebackup.sh
@@ -5,55 +5,59 @@ eval set -- "$PARSED"
 
 RETRIES=2
 
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --datadir )
-            DATA_DIR=$2
-            shift
+while getopts ":-:" optchar; do
+    [[ "${optchar}" == "-" ]] || continue
+    case "${OPTARG}" in
+        datadir=* )
+            DATA_DIR=${OPTARG#*=}
             ;;
-        --connstring )
-            CONNSTR=$2
-            shift
+        connstring=* )
+            CONNSTR=${OPTARG#*=}
             ;;
-        --retries )
-            RETRIES=$2
-            shift
-            ;;
-        * )
+        retries=* )
+            RETRIES=${OPTARG#*=}
             ;;
     esac
-    shift
 done
 
 [[ -z $DATA_DIR || -z $CONNSTR || ! $RETRIES =~ ^[1-9]$ ]] && exit 1
 
-function receivexlog() {
-    pg_receivexlog --directory="${XLOG_FAST}" --dbname="${CONNSTR}" &
-    receivexlog_pid=$!
+function receivewal() {
+    if which pg_receivewal &> /dev/null; then
+        PG_RECEIVEWAL=pg_receivewal
+    else
+        PG_RECEIVEWAL=pg_receivexlog
+    fi
+    $PG_RECEIVEWAL --directory="${WAL_FAST}" --dbname="${CONNSTR}" &
+    receivewal_pid=$!
 
-    # run pg_receivexlog until postgres will not start streaming
+    # run pg_receivewal until postgres will not start streaming
     while ! ps ax | grep -qE '[w]al receiver process\s+streaming'; do
-        # exit if pg_receivexlog is not running
-        kill -0 $receivexlog_pid && sleep 1 || exit
+        # exit if pg_receivewal is not running
+        kill -0 $receivewal_pid && sleep 1 || exit
     done
 
-    kill $receivexlog_pid && sleep 1
-    rm -f ${XLOG_FAST}/*
+    kill $receivewal_pid && sleep 1
+    rm -f ${WAL_FAST}/*
 }
 
 ATTEMPT=0
 while [[ $((ATTEMPT++)) -le $RETRIES ]]; do
     rm -fr "${DATA_DIR}"
-    pg_basebackup --pgdata="${DATA_DIR}" --xlog-method=stream --dbname="${CONNSTR}"
+    pg_basebackup --pgdata="${DATA_DIR}" -X stream --dbname="${CONNSTR}"
     EXITCODE=$?
     if [[ $EXITCODE == 0 ]]; then
-        XLOG_FAST=$(dirname $DATA_DIR)/xlog_fast
-        rm -fr $XLOG_FAST
-        mv ${DATA_DIR}/pg_xlog $XLOG_FAST
-        rm -fr $XLOG_FAST/archive_status
-        mkdir ${DATA_DIR}/pg_xlog
+        WAL_FAST=$(dirname $DATA_DIR)/wal_fast
 
-        receivexlog &
+        WAL_DIR=${DATA_DIR}/pg_wal
+        [[ -d ${WAL_DIR} ]] || WAL_DIR=${DATA_DIR}/pg_xlog
+
+        rm -fr $WAL_FAST $WAL_DIR/archive_status
+
+        mv $WAL_DIR $WAL_FAST
+        mkdir $WAL_DIR
+
+        receivewal &
         break
     elif [[ $ATTEMPT -le $RETRIES ]]; then
         sleep $((ATTEMPT*10))
