5f0549151f3155577d42e6c62d4a297cd609a461
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2017-11-17 09:42:39 +0100

    xlog => wal rename for consistency

--- postgres-appliance/wale_restore_command.sh
+++ postgres-appliance/wale_restore_command.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-readonly xlog_filename=$1
-readonly xlog_destination=$2
+readonly wal_filename=$1
+readonly wal_destination=$2
 
-[[ -z $xlog_filename || -z $xlog_destination ]] && exit 1
+[[ -z $wal_filename || -z $wal_destination ]] && exit 1
 
-readonly xlog_dir=$(dirname $xlog_destination)
-readonly xlog_fast_source=$(dirname $(dirname $(realpath $xlog_dir)))/xlog_fast/$xlog_filename
+readonly wal_dir=$(dirname $wal_destination)
+readonly wal_fast_source=$(dirname $(dirname $(realpath $wal_dir)))/wal_fast/$wal_filename
 
-[[ -f $xlog_fast_source ]] && exec mv "${xlog_fast_source}" "${xlog_destination}"
+[[ -f $wal_fast_source ]] && exec mv "${wal_fast_source}" "${wal_destination}"
 
-POOL_SIZE=$(($(nproc)-1))
+POOL_SIZE=$(($(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)-1))
 [[ $POOL_SIZE -gt 8 ]] && POOL_SIZE=8
 
 if [[ -z $WALE_S3_PREFIX ]]; then  # non AWS environment?
-    readonly wale_prefetch_source=${xlog_dir}/.wal-e/prefetch/${xlog_filename}
+    readonly wale_prefetch_source=${wal_dir}/.wal-e/prefetch/${wal_filename}
     if [[ -f $wale_prefetch_source ]]; then
-        exec mv "${wale_prefetch_source}" "${xlog_destination}"
+        exec mv "${wale_prefetch_source}" "${wal_destination}"
     else
-        exec wal-e wal-fetch -p $POOL_SIZE "${xlog_filename}" "${xlog_destination}"
+        exec wal-e wal-fetch -p $POOL_SIZE "${wal_filename}" "${wal_destination}"
     fi
 else
-    exec /wal-e-wal-fetch.sh --aws-instance-profile wal-fetch -p $POOL_SIZE "${xlog_filename}" "${xlog_destination}"
+    exec /wal-e-wal-fetch.sh --aws-instance-profile wal-fetch -p $POOL_SIZE "${wal_filename}" "${wal_destination}"
 fi
