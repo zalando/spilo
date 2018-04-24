157dd5856812b89ad92d6b65c5c7e213040b97a0
Sergey Dudoladov <sergey.dudoladov@zalando.de>
2018-04-24 12:31:11 +0200

    Install awscli and use sync instead of copy

--- postgres-appliance/ship_daily_pg_logs_to_s3.sh
+++ postgres-appliance/ship_daily_pg_logs_to_s3.sh
@@ -1,4 +1,6 @@
-# the BusyBox version of `date` installed in Spilo lacks the ' --date "yesterday" ' option, so we process the yesterday's date in code
+#!/bin/bash
+
+# the BusyBox version of `date` installed in Spilo lacks the ' --date "yesterday" ' option, so we process the date in code
 
 # determine the file to upload; assume Monday is 1, Sunday is 7
 yesterday_day_number=$(($(date +%u) - 1)) 
@@ -11,14 +13,16 @@ log_file="${PGLOG}/postgresql-${yesterday_day_number}.csv"
 # if a pod was started today, yesterday's logs will be empty
 if [ -s "$log_file" ];
 then
-# TODO fix S3 bucket
+
   # get the yesterday's date from the log itself
   yesterday=$(head -1 "$log_file" | cut -d " " -f 1)
 
-  # compress with gzip under descriptive name and upload
-  archive_name_with_date="${PGLOG}/postgresql-"$yesterday"-test.csv.gz"
+  archive_name_with_date="${PGLOG}/postgresql-"$yesterday".csv.gz"
   tar cz -f "$archive_name_with_date" "$log_file"
-  aws s3 cp "$archive_name_with_date" s3://"$WAL_S3_BUCKET"
+
+  # upload file if it does not exist in the bucket
+  # exclude/include filters - in that particular order - ensure the operation syncs only a single file
+  aws s3 sync ${PGLOG} s3://"$WAL_S3_BUCKET" --exclude '*' --include "'${archive_name_with_date}'"
   rm "$archive_name_with_date"
 
 fi
