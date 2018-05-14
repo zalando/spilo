17c9180f09195ae619ae1934f659f758807dc241
Sergey Dudoladov <sergey.dudoladov@zalando.de>
2018-05-14 15:54:11 +0200

    Remove authentication (boto does it automaticallly)

--- postgres-appliance/upload_pg_log_to_s3.py
+++ postgres-appliance/upload_pg_log_to_s3.py
@@ -27,8 +27,7 @@ def upload_to_s3(local_file_path):
 
     # specifying the region also sets the correct AWS SignatureVersion
     # see https://github.com/boto/boto/issues/2741
-    key_id, key, token = fetch_aws_credentials()
-    conn = boto.connect_s3(key_id, key, host = os.getenv('LOG_S3_HOST'), security_token = token)
+    conn = boto.connect_s3(host = os.getenv('LOG_S3_HOST'))
 
     bucket_name = os.getenv('LOG_S3_BUCKET')
     bucket = conn.get_bucket(bucket_name, validate = False)
@@ -36,15 +35,6 @@ def upload_to_s3(local_file_path):
     object_to_upload = boto.s3.key.Key(bucket)
     object_to_upload.key = os.getenv('LOG_S3_KEY') + '/' + os.path.basename(local_file_path)
     object_to_upload.set_contents_from_filename(local_file_path)
-
-
-def fetch_aws_credentials():
-
-    credentials_url = "http://169.254.169.254/latest/meta-data/iam/security-credentials/"
-    instance_profile = requests.get(credentials_url).content
-
-    credentials = requests.get(credentials_url + instance_profile.decode("utf-8")).json()
-    return credentials['AccessKeyId'], credentials['SecretAccessKey'], credentials['Token']
 
 
 def main():
