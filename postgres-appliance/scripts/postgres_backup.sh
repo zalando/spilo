72c6775698706066dac6c3a91dde56ce59381998
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2018-06-07 12:44:38 +0200

    Address review

--- postgres-appliance/scripts/postgres_backup.sh
+++ postgres-appliance/scripts/postgres_backup.sh
@@ -12,13 +12,13 @@ log "I was called as: $0 $@"
 
 readonly WALE_ENV_DIR=$1
 readonly PGDATA=$2
-NUM_TO_RETAIN=$3
+DAYS_TO_RETAIN=$3
 
 readonly IN_RECOVERY=$(psql -tXqAc "select pg_is_in_recovery()")
 [[ $IN_RECOVERY != "f" ]] && log "Cluster is in recovery, not running backup" && exit 0
 
 # leave at least 2 days base backups before creating a new one
-[[ "$NUM_TO_RETAIN" -lt 2 ]] && NUM_TO_RETAIN=2
+[[ "$DAYS_TO_RETAIN" -lt 2 ]] && DAYS_TO_RETAIN=2
 
 readonly WAL_E="envdir $WALE_ENV_DIR wal-e --aws-instance-profile"
 
@@ -27,8 +27,8 @@ BEFORE=""
 readonly NOW=$(date +%s -u)
 while read name last_modified rest; do
     last_modified=$(date +%s -ud "$last_modified")
-    if [ $(((NOW-last_modified)/86400)) -gt $NUM_TO_RETAIN ]; then
-        if [ -z "$BEFORE" ] || [ "$last_modified" -lt "$BEFORE_TIME" ]; then
+    if [ $(((NOW-last_modified)/86400)) -gt $DAYS_TO_RETAIN ]; then
+        if [ -z "$BEFORE" ] || [ "$last_modified" -gt "$BEFORE_TIME" ]; then
             BEFORE_TIME=$last_modified
             BEFORE=$name
         fi
