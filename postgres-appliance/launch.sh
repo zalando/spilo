fd579c00b7895c8b12a8df746a667e7921b9d319
ToroNZ <tomas+github@libre.nz>
2020-02-26 12:09:10 +1300

    removed extra spaces

--- postgres-appliance/launch.sh
+++ postgres-appliance/launch.sh
@@ -39,9 +39,9 @@ chmod 01777 "$RW_DIR/tmp"
 if [ "$DEMO" = "true" ]; then
     python3 /scripts/configure_spilo.py patroni pgqd certificate pam-oauth2
 elif [ "$(id -u)" -ne 0 ] && python3 /scripts/configure_spilo.py all; then
-        PATH=$PATH /scripts/patroni_wait.sh -t 3600 -- envdir $WALE_ENV_DIR /scripts/postgres_backup.sh $PGDATA &
+    PATH=$PATH /scripts/patroni_wait.sh -t 3600 -- envdir $WALE_ENV_DIR /scripts/postgres_backup.sh $PGDATA &
 elif python3 /scripts/configure_spilo.py all; then
-        su postgres -c "PATH=$PATH /scripts/patroni_wait.sh -t 3600 -- envdir $WALE_ENV_DIR /scripts/postgres_backup.sh $PGDATA" &
+    su postgres -c "PATH=$PATH /scripts/patroni_wait.sh -t 3600 -- envdir $WALE_ENV_DIR /scripts/postgres_backup.sh $PGDATA" &
 fi
 
 sv_stop() {
