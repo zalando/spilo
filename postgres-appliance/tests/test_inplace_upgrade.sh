cf37c6d18c7063306bb162ffca7300c3e71afe19
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-08-28 09:57:28 +0200

    Polish tests

--- postgres-appliance/tests/test_inplace_upgrade.sh
+++ postgres-appliance/tests/test_inplace_upgrade.sh
@@ -5,6 +5,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 readonly PREFIX="demo-"
 readonly UPGRADE_SCRIPT="python3 /scripts/inplace_upgrade.py"
 
+if [[ -t 2 ]]; then
+    readonly RED="\033[1;31m"
+    readonly RESET="\033[0m"
+    readonly GREEN="\033[0;32m"
+else
+    readonly RED=""
+    readonly RESET=""
+    readonly GREEN=""
+fi
+
 function start_containers() {
     docker-compose up -d
 }
@@ -81,13 +91,13 @@ function create_schema() {
 }
 
 function drop_table_with_oids() {
-    docker_exec -i "$1" "psql -U postgres -d test_db -c 'DROP TABLE with_oids'"
+    docker_exec "$1" "psql -U postgres -d test_db -c 'DROP TABLE with_oids'"
 }
 
 function test_upgrade_wrong_container() {
     local container
     container=$(get_non_leader "$1")
-    docker_exec "$container" "PGVERSION=10 $UPGRADE_SCRIPT 4"
+    ! docker_exec "$container" "PGVERSION=10 $UPGRADE_SCRIPT 4"
 }
 
 function test_upgrade_wrong_version() {
@@ -98,39 +108,51 @@ function test_upgrade_wrong_capacity() {
     docker_exec "$1" "PGVERSION=10 $UPGRADE_SCRIPT 4" 2>&1 | grep 'number of replicas does not match'
 }
 
-function test_successful_upgrade() {
+function test_successful_upgrade_to_10() {
     docker_exec "$1" "PGVERSION=10 $UPGRADE_SCRIPT 3"
 }
 
-function test_upgrade_12() {
+function test_failed_upgrade_big_replication_lag() {
+    ! test_successful_upgrade_to_10 "$1"
+}
+
+function test_successful_upgrade_to_12() {
     docker_exec "$1" "PGVERSION=12 $UPGRADE_SCRIPT 3"
 }
 
-function test_pg_upgrade_check() {
-    test_upgrade_12 "$1"
+function test_pg_upgrade_check_failed() {
+    ! test_successful_upgrade_to_12 "$1"
+}
+
+function run_test() {
+    if ! "$@"; then
+        echo -e "${RED}Test case $1 FAILED${RESET}"
+        exit 1
+    fi
+    echo -e "Test case $1 ${GREEN}PASSED${RESET}"
 }
 
 function test_upgrade() {
     local container=$1
 
-    test_upgrade_wrong_version "$container" || exit 1
-    test_upgrade_wrong_capacity "$container" || exit 1
+    run_test test_upgrade_wrong_version "$container"
+    run_test test_upgrade_wrong_capacity "$container"
 
     wait_all_streaming "$container"
 
-    test_upgrade_wrong_container "$container" && exit 1
+    run_test test_upgrade_wrong_container "$container"
 
     create_schema "$container" || exit 1
-    test_successful_upgrade "$container" && exit 1  # should fail due to the lag
+    run_test test_failed_upgrade_big_replication_lag "$container"
 
     wait_zero_lag "$container"
-    test_successful_upgrade "$container" || exit 1
+    run_test test_successful_upgrade_to_10 "$container"
 
     wait_all_streaming "$container"
-    test_pg_upgrade_check "$container" && exit 1  # pg_upgrade --check complains about OID
+    run_test test_pg_upgrade_check_failed "$container"  # pg_upgrade --check complains about OID
 
     drop_table_with_oids "$container"
-    test_upgrade_12 "$container" || exit 1
+    run_test test_successful_upgrade_to_12 "$container"
 }
 
 function main() {
