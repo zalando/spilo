f84531923e6ab91758e030df13a2b4ffe978b64b
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-09-23 16:07:11 +0200

    Disable prefetch when restore_command is called by pg_rewind
    * WALG_DOWNLOAD_CONCURRENCY=1 env for wal-g
    * -p 1 option for wal-e

--- postgres-appliance/scripts/restore_command.sh
+++ postgres-appliance/scripts/restore_command.sh
@@ -10,18 +10,21 @@ readonly wal_fast_source=$(dirname "$(dirname "$(realpath "$wal_dir")")")/wal_fa
 
 [[ -f $wal_fast_source ]] && exec mv "${wal_fast_source}" "${wal_destination}"
 
+if [[ "$wal_destination" =~ /$wal_filename$ ]]; then  # Patroni fetching missing files for pg_rewind
+    export WALG_DOWNLOAD_CONCURRENCY=1
+fi
+
 [[ "$USE_WALG_RESTORE" == "true" ]] && exec wal-g wal-fetch "${wal_filename}" "${wal_destination}"
 
-POOL_SIZE=$(($(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)-1))
-[[ $POOL_SIZE -gt 8 ]] && POOL_SIZE=8
+[[ $WALG_DOWNLOAD_CONCURRENCY -gt 8 ]] && WALG_DOWNLOAD_CONCURRENCY=8
 
 if [[ -z $WALE_S3_PREFIX ]]; then  # non AWS environment?
     readonly wale_prefetch_source=${wal_dir}/.wal-e/prefetch/${wal_filename}
     if [[ -f $wale_prefetch_source ]]; then
         exec mv "${wale_prefetch_source}" "${wal_destination}"
     else
-        exec wal-e wal-fetch -p $POOL_SIZE "${wal_filename}" "${wal_destination}"
+        exec wal-e wal-fetch -p $WALG_DOWNLOAD_CONCURRENCY "${wal_filename}" "${wal_destination}"
     fi
 else
-    exec bash /scripts/wal-e-wal-fetch.sh wal-fetch -p $POOL_SIZE "${wal_filename}" "${wal_destination}"
+    exec bash /scripts/wal-e-wal-fetch.sh wal-fetch -p $WALG_DOWNLOAD_CONCURRENCY "${wal_filename}" "${wal_destination}"
 fi
