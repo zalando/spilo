4e80637577eb5569007c5d505a4812ac50d40aab
Steve Singer <steve@ssinger.info>
2020-11-23 09:52:42 -0500

    Update postgres-appliance/scripts/configure_spilo.py
    Co-authored-by: Alexander Kukushkin <cyberdemn@gmail.com>

--- postgres-appliance/scripts/configure_spilo.py
+++ postgres-appliance/scripts/configure_spilo.py
@@ -700,8 +700,7 @@ def write_wale_environment(placeholders, prefix, overwrite):
     wale = defaultdict(lambda: '')
     for name in ['WALE_ENV_DIR', 'SCOPE', 'WAL_BUCKET_SCOPE_PREFIX', 'WAL_BUCKET_SCOPE_SUFFIX',
                  'WAL_S3_BUCKET', 'WAL_GCS_BUCKET', 'WAL_GS_BUCKET', 'WAL_SWIFT_BUCKET', 'BACKUP_NUM_TO_RETAIN',
-                 'WALG_AZ_PREFIX'] +\
-            s3_names + swift_names + gs_names + walg_names + azure_names:
+                 'WALG_AZ_PREFIX'] + s3_names + swift_names + gs_names + walg_names + azure_names:
         wale[name] = placeholders.get(prefix + name, '')
 
     if wale.get('WAL_S3_BUCKET') or wale.get('WALE_S3_PREFIX') or wale.get('WALG_S3_PREFIX'):
