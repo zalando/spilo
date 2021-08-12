0ab7fe0178237fea341ea154c04e03c854596655
irishandyb <irishandyb@gmail.com>
2021-08-12 14:52:59 +0100

    #585 certificate rotation based on file hash, not modified time (#599)
    Close https://github.com/zalando/spilo/issues/585

--- postgres-appliance/scripts/configure_spilo.py
+++ postgres-appliance/scripts/configure_spilo.py
@@ -855,9 +855,10 @@ def write_crontab(placeholders, overwrite):
         logging.info('Skipping creation of renice cron job due to lack of SYS_NICE capability')
 
     if placeholders.get('SSL_TEST_RELOAD'):
-        env = ' '.join('{0}="{1}"'.format(n, placeholders[n]) for n in ('SSL_CA_FILE', 'SSL_CRL_FILE',
+        env = ' '.join('{0}="{1}"'.format(n, placeholders[n]) for n in ('PGDATA', 'SSL_CA_FILE', 'SSL_CRL_FILE',
                        'SSL_CERTIFICATE_FILE', 'SSL_PRIVATE_KEY_FILE') if placeholders.get(n))
-        lines += ['*/5 * * * * {0} /scripts/test_reload_ssl.sh 5'.format(env)]
+        hash_dir = os.path.join(placeholders['RW_DIR'], 'tmp')
+        lines += ['*/5 * * * * {0} /scripts/test_reload_ssl.sh {1}'.format(env, hash_dir)]
 
     if bool(placeholders.get('USE_WALE')):
         lines += [('{BACKUP_SCHEDULE} envdir "{WALE_ENV_DIR}" /scripts/postgres_backup.sh' +
