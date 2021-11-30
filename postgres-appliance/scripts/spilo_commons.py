4d3edb620c804471a186787f1657343cfb8d12b7
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2021-11-30 12:50:39 +0100

    Enable timescaledb on v14
    Close https://github.com/zalando/spilo/issues/666

--- postgres-appliance/scripts/spilo_commons.py
+++ postgres-appliance/scripts/spilo_commons.py
@@ -12,7 +12,7 @@ LIB_DIR = '/usr/lib/postgresql'
 
 # (min_version, max_version, shared_preload_libraries, extwlist.extensions)
 extensions = {
-    'timescaledb':    (9.6, 13, True,  True),
+    'timescaledb':    (9.6, 14, True,  True),
     'pg_cron':        (9.5, 14, True,  False),
     'pg_stat_kcache': (9.4, 14, True,  False),
     'pg_partman':     (9.4, 14, False, True)
