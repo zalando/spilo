0a0ef0617ccd0937a51cb5e2f4703870a3749ecc
Polina Bungina <polina.bungina@zalando.de>
2022-08-30 16:09:09 +0200

    Make it possible to run dependencies build script with podman

--- postgres-appliance/dependencies/build.sh
+++ postgres-appliance/dependencies/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! docker info &> /dev/null; then
+    if podman info &> /dev/null; then
+        alias docker=podman
+        shopt -s expand_aliases
+    else
+        echo "docker/podman: command not found"
+        exit 1
+    fi
+fi
+
 set -ex
 
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
