67ce6d8629b090f30e19c95dc30589d3f6ce42d3
Polina Bungina <polina.bungina@zalando.de>
2022-08-31 07:25:37 +0200

    Change libgdal version in a comment

--- postgres-appliance/build_scripts/dependencies.sh
+++ postgres-appliance/build_scripts/dependencies.sh
@@ -36,7 +36,7 @@ export USE_LIBSODIUM=1
 export USE_LZO=1
 make pg_build
 
-# We want to remove all libgdal20 debs except one that is for current architecture.
+# We want to remove all libgdal30 debs except one that is for current architecture.
 printf "shopt -s extglob\nrm /builddeps/!(*_%s.deb)" "$ARCH" | bash -s
 
 mkdir /builddeps/wal-g
