86a872bf99168891f8449b80190e57d310ab8045
Polina Bungina <bungina@gmail.com>
2023-01-13 13:37:09 +0100

    Enforce search_path=pg_catalog for post_init and wale_restore scripts (#826)

--- postgres-appliance/scripts/on_role_change.sh
+++ postgres-appliance/scripts/on_role_change.sh
@@ -10,7 +10,7 @@ readonly dbname=postgres
 if [[ "${*: -3:1}" == "on_role_change" && "${*: -2:1}" == "master" ]]; then
     num=30  # wait 30 seconds for end of recovery
     while  [[ $((num--)) -gt 0 ]]; do
-        if [[ "$(psql -d $dbname -tAc 'SELECT pg_is_in_recovery()')" == "f" ]]; then
+        if [[ "$(psql -d $dbname -tAc 'SELECT pg_catalog.pg_is_in_recovery()')" == "f" ]]; then
             vacuumdb -aZ > /dev/null 2>&1 &
             exec /scripts/post_init.sh "$HUMAN_ROLE" "$dbname"
         else
