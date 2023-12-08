952d151615554176e8fb9e000f80fb2998c638cd
Jociele Padilha <45459238+jopadi@users.noreply.github.com>
2023-12-08 14:34:50 +0100

    add timescaledb version 2.13.0 to PG16 (#951)
    * add timescaledb version 2.13.0 to PG16
    
    * remove old timescale and add the pg16 to spilo_commons
    
    * adjust timescale
    
    * comment tests to skip the timescale for pg16

--- postgres-appliance/scripts/spilo_commons.py
+++ postgres-appliance/scripts/spilo_commons.py
@@ -12,7 +12,7 @@ LIB_DIR = '/usr/lib/postgresql'
 
 # (min_version, max_version, shared_preload_libraries, extwlist.extensions)
 extensions = {
-    'timescaledb':    (9.6, 15, True,  True),
+    'timescaledb':    (9.6, 16, True,  True),
     'pg_cron':        (9.5, 16, True,  False),
     'pg_stat_kcache': (9.4, 16, True,  False),
     'pg_partman':     (9.4, 16, False, True)
