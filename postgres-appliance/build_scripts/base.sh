b20dc7d405f6aa344e34610a1934c61a5f652b63
Maxim Kurbatov <max@cubly.ru>
2024-02-24 03:50:07 +0500

    Add requried pgvector SQLs to image

--- postgres-appliance/build_scripts/base.sh
+++ postgres-appliance/build_scripts/base.sh
@@ -174,6 +174,8 @@ for version in $DEB_PG_SUPPORTED_VERSIONS; do
             fi
             cargo pgrx init "--pg${version}=/usr/lib/postgresql/${version}/bin/pg_config"
             cargo pgrx install --release
+            cp sql/install/vectors--${PGVECTORS_VERSION}.sql /usr/share/postgresql/${version}/extension/ &&\
+            cp sql/upgrade/*.sql /usr/share/postgresql/${version}/extension/
         )
     fi
     # pgvecto.rs end
