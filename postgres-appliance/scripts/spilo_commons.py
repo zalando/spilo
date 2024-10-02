2f6a940363690fdaedf098e5947c34023ad732c7
idanovinda <idanovinda@gmail.com>
2024-10-02 09:22:15 +0200

    update version in spilo_commons.py

--- postgres-appliance/scripts/spilo_commons.py
+++ postgres-appliance/scripts/spilo_commons.py
@@ -15,7 +15,7 @@ extensions = {
     'timescaledb':    (9.6, 16, True,  True),
     'pg_cron':        (9.5, 17, True,  False),
     'pg_stat_kcache': (9.4, 17, True,  False),
-    'pg_partman':     (9.4, 16, False, True)
+    'pg_partman':     (9.4, 17, False, True)
 }
 if os.environ.get('ENABLE_PG_MON') == 'true':
     extensions['pg_mon'] = (11,  17, True,  False)
