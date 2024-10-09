f6a9c6bcf5cdca3f7882a6526bc586797ee6fc57
Felix Kunde <felix-kunde@gmx.de>
2024-10-09 11:36:32 +0200

    fill s3 tags before copying to log env

--- postgres-appliance/scripts/configure_spilo.py
+++ postgres-appliance/scripts/configure_spilo.py
@@ -787,6 +787,9 @@ def write_log_environment(placeholders):
     if not os.path.exists(log_env['LOG_ENV_DIR']):
         os.makedirs(log_env['LOG_ENV_DIR'])
 
+    tags = eval(os.getenv('LOG_S3_TAGS'))
+    log_env['LOG_S3_TAGS'] = "&".join(f"{key}={os.getenv(value)}" for key, value in tags.items())
+
     for var in ('LOG_TMPDIR',
                 'LOG_SHIP_HOURLY',
                 'LOG_AWS_REGION',
