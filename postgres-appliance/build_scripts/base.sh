bfc3d54e8b18e371725b1fb655c1189172c4fd14
Ida Novindasari <idanovinda@gmail.com>
2024-11-11 16:42:59 +0100

    Only install the latest compatible version of timescaledb (#1046)
    Co-authored-by: Polina Bungina <27892524+hughcapet@users.noreply.github.com>

--- postgres-appliance/build_scripts/base.sh
+++ postgres-appliance/build_scripts/base.sh
@@ -124,6 +124,19 @@ for version in $DEB_PG_SUPPORTED_VERSIONS; do
         "postgresql-${version}-pg-stat-kcache" \
         "${EXTRAS[@]}"
 
+    # Clean up timescaledb versions except the highest compatible version
+    exclude_patterns=()
+    exclude_patterns_tsl=()
+    for ts_version in ${TIMESCALEDB}; do
+        exclude_patterns+=(! -name timescaledb-"${ts_version}".so)
+        exclude_patterns_tsl+=(! -name timescaledb-tsl-"${ts_version}".so)
+    done
+    find /usr/lib/postgresql/"${version}"/lib/ -name 'timescaledb-2.*.so' "${exclude_patterns[@]}" -delete;
+
+    if [ "${TIMESCALEDB_APACHE_ONLY}" != "true" ]; then
+        find /usr/lib/postgresql/"${version}"/lib/ -name 'timescaledb-tsl-2.*.so' "${exclude_patterns_tsl[@]}" -delete;
+    fi
+
     # Install 3rd party stuff
 
     if [ "${TIMESCALEDB_APACHE_ONLY}" != "true" ] && [ "${TIMESCALEDB_TOOLKIT}" = "true" ]; then
