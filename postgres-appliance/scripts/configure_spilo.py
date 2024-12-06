518d2f6e7bf517f86d1c371d134b9b8476318fa0
Ida Novindasari <idanovinda@gmail.com>
2024-12-06 15:25:47 +0100

    Integrate Patroni 4.x (#1050)
    * Integrate Patroni v4.x
    * set docs
    * update to latest patroni version

--- postgres-appliance/scripts/configure_spilo.py
+++ postgres-appliance/scripts/configure_spilo.py
@@ -578,6 +578,8 @@ def get_placeholders(provider):
     placeholders.setdefault('CALLBACK_SCRIPT', '')
     placeholders.setdefault('DCS_ENABLE_KUBERNETES_API', '')
     placeholders.setdefault('KUBERNETES_ROLE_LABEL', 'spilo-role')
+    placeholders.setdefault('KUBERNETES_LEADER_LABEL_VALUE', 'master')
+    placeholders.setdefault('KUBERNETES_STANDBY_LEADER_LABEL_VALUE', 'master')
     placeholders.setdefault('KUBERNETES_SCOPE_LABEL', 'version')
     placeholders.setdefault('KUBERNETES_LABELS', KUBERNETES_DEFAULT_LABELS)
     placeholders.setdefault('KUBERNETES_USE_CONFIGMAPS', '')
