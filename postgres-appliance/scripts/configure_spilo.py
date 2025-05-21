7b9d2f660ca87dab254e5df579a6d7ae1ce14002
Polina Bungina <polina.bungina@zalando.de>
2025-05-21 17:46:43 +0200

    Write AWS_EC2_METADATA_SERVICE_ENDPOINT* env files only when actually needed

--- postgres-appliance/scripts/configure_spilo.py
+++ postgres-appliance/scripts/configure_spilo.py
@@ -813,8 +813,14 @@ def write_log_environment(placeholders):
 
     log_env['LOG_S3_TAGS'] = "&".join(f"{key}={os.getenv(value)}" for key, value in tags.items())
     # support for older boto3 versions: https://github.com/boto/botocore/pull/2600
-    if not log_env['AWS_EC2_METADATA_SERVICE_ENDPOINT'].endswith('/'):
+    if ('AWS_EC2_METADATA_SERVICE_ENDPOINT' in log_env
+            and not log_env['AWS_EC2_METADATA_SERVICE_ENDPOINT'].endswith('/')):
         log_env['AWS_EC2_METADATA_SERVICE_ENDPOINT'] += '/'
+        write_file(log_env['AWS_EC2_METADATA_SERVICE_ENDPOINT'],
+                   os.path.join(log_env['LOG_ENV_DIR'], 'AWS_EC2_METADATA_SERVICE_ENDPOINT'), True)
+    if 'AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE' in log_env:
+        write_file(log_env['AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE'],
+                   os.path.join(log_env['LOG_ENV_DIR'], 'AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE'), True)
 
     for var in ('LOG_TMPDIR',
                 'LOG_SHIP_HOURLY',
@@ -823,9 +829,7 @@ def write_log_environment(placeholders):
                 'LOG_S3_KEY',
                 'LOG_S3_BUCKET',
                 'LOG_S3_TAGS',
-                'PGLOG',
-                'AWS_EC2_METADATA_SERVICE_ENDPOINT',
-                'AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE',):
+                'PGLOG',):
         write_file(log_env[var], os.path.join(log_env['LOG_ENV_DIR'], var), True)
 
 
