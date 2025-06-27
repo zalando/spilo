1955d42f2d4911046326bf6ff0eae8e55624b01b
idanovinda <idanovinda@gmail.com>
2025-06-27 15:21:48 +0200

    make sure deleted backup is older than DAYS_TO_RETAIN

--- postgres-appliance/scripts/postgres_backup.sh
+++ postgres-appliance/scripts/postgres_backup.sh
@@ -11,6 +11,7 @@ log "I was called as: $0 $*"
 
 
 readonly PGDATA=$1
+DAYS_TO_RETAIN=$BACKUP_NUM_TO_RETAIN
 
 IN_RECOVERY=$(psql -tXqAc "select pg_catalog.pg_is_in_recovery()")
 readonly IN_RECOVERY
@@ -44,19 +45,35 @@ nice -n 5 $WAL_E backup-push "$PGDATA" "${POOL_SIZE[@]}"
 mapfile -t backup_records < <(wal-g backup-list 2>/dev/null |
     sed '0,/^\(backup_\)\?name\s*\(last_\)\?modified\s*/d' |
     sort -k2r  | 
-    awk '{ print $1 }'
+    awk '{ print $1, $2 }'
     )
+
+# leave at least 2 days base backups and/or 2 backups
+[[ "$BACKUP_NUM_TO_RETAIN" -lt 2 ]] && BACKUP_NUM_TO_RETAIN=2
+[[ "$DAYS_TO_RETAIN" -lt 2 ]] && DAYS_TO_RETAIN=2
 
 # Compute total after collection
 TOTAL=${#backup_records[@]}
+BEFORE=""
+NOW=$(date +%s -u)
+readonly NOW
+index=$(( BACKUP_NUM_TO_RETAIN - 1 ))
 
-# leave at least 2 days base backups
-[[ "$BACKUP_NUM_TO_RETAIN" -lt 2 ]] && BACKUP_NUM_TO_RETAIN=2
+while [[ $index -lt $TOTAL ]]; do
+    last_modified=$(date +%s -ud "${backup_records[$index]##* }")
+    if (( (NOW - last_modified) / 86400 >= DAYS_TO_RETAIN )); then
+        BEFORE="${backup_records[$index]%% *}"
+        break
+    fi
+    index=$(( index + 1 ))
+done
 
-# get the date of the BACKUP_NUM_TO_RETAIN-th newest backup
+if [[ -z $BEFORE ]]; then
+    log "No backups older than $DAYS_TO_RETAIN days found, not deleting any"
+    exit 0
+fi
+
 if [[ $TOTAL -gt $BACKUP_NUM_TO_RETAIN ]]; then
-    # check the current tool used for backups
-    BEFORE="${backup_records[$BACKUP_NUM_TO_RETAIN-1]}"
     wal-g delete before FIND_FULL "$BEFORE" --confirm
 else
     log "There are only $TOTAL backups, not deleting any"
