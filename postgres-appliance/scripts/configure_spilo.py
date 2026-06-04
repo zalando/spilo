8ded2d06f1e5ffabc93d0dd693b5e3e6fa9d8afa
Felix Kunde <felix-kunde@gmx.de>
2026-06-04 15:45:50 +0200

    replace md5 with scram-sha-256

--- postgres-appliance/scripts/configure_spilo.py
+++ postgres-appliance/scripts/configure_spilo.py
@@ -327,13 +327,13 @@ hstore,hypopg,intarray,ltree,pgcrypto,pgq,pgq_node,pg_trgm,postgres_fdw,roaringb
     {{#PAM_OAUTH2}}
     - hostssl all             +{{HUMAN_ROLE}}    127.0.0.1/32       pam
     {{/PAM_OAUTH2}}
-    - host    all             all                127.0.0.1/32       md5
+    - host    all             all                127.0.0.1/32       scram-sha-256
     {{#PAM_OAUTH2}}
     - hostssl all             +{{HUMAN_ROLE}}    ::1/128            pam
     {{/PAM_OAUTH2}}
-    - host    all             all                ::1/128            md5
+    - host    all             all                ::1/128            scram-sha-256
     - local   replication     {{PGUSER_STANDBY}}                    trust
-    - hostssl replication     {{PGUSER_STANDBY}} all                md5
+    - hostssl replication     {{PGUSER_STANDBY}} all                scram-sha-256
     {{^ALLOW_NOSSL}}
     - hostnossl all           all                all                reject
     {{/ALLOW_NOSSL}}
@@ -341,10 +341,10 @@ hstore,hypopg,intarray,ltree,pgcrypto,pgq,pgq_node,pg_trgm,postgres_fdw,roaringb
     - hostssl all             +{{HUMAN_ROLE}}    all                pam
     {{/PAM_OAUTH2}}
     {{#ALLOW_NOSSL}}
-    - host    all             all                all                md5
+    - host    all             all                all                scram-sha-256
     {{/ALLOW_NOSSL}}
     {{^ALLOW_NOSSL}}
-    - hostssl all             all                all                md5
+    - hostssl all             all                all                scram-sha-256
     {{/ALLOW_NOSSL}}
 
   {{#USE_WALG}}
@@ -1171,7 +1171,7 @@ def main():
 
     # Ensure replication is available
     if 'pg_hba' in config['bootstrap'] and not any(['replication' in i for i in config['bootstrap']['pg_hba']]):
-        rep_hba = 'hostssl replication {} all md5'.\
+        rep_hba = 'hostssl replication {} all scram-sha-256'.\
             format(config['postgresql']['authentication']['replication']['username'])
         config['bootstrap']['pg_hba'].insert(0, rep_hba)
 
